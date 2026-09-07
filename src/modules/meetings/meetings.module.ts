472e5b0cc61d1fb1c5a40d6c6246913994bcabe7
Aryan8Byte <aryan.g@8byte.ai>
2026-09-07 15:34:23 +0530

    feat(meetings): import intelligence module into meetings module

--- src/modules/meetings/meetings.module.ts
+++ src/modules/meetings/meetings.module.ts
@@ -2,11 +2,16 @@ import { Module, forwardRef } from '@nestjs/common';
 import { MeetingsController } from './meetings.controller';
 import { MeetingsService } from './meetings.service';
 import { IntegrationsModule } from '../integrations/integrations.module';
+import { IntelligenceModule } from '../intelligence/intelligence.module';
 
 @Module({
-  imports: [forwardRef(() => IntegrationsModule)],
+  imports: [
+    forwardRef(() => IntegrationsModule),
+    forwardRef(() => IntelligenceModule),
+  ],
   controllers: [MeetingsController],
   providers: [MeetingsService],
   exports: [MeetingsService],
 })
 export class MeetingsModule {}
+
